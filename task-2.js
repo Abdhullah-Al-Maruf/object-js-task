6758ba84f754dc51422ed34736020812245c55a8
Abdhullah-Al-Maruf <mdmaruf20053@gmail.com>
2026-02-05 10:43:13 +0600

    task2 solution added

---
+++ task-2.js
@@ -0,0 +1,17 @@
+// ### Task-2
+// For this object below add a property named `passenger capacity` with value 5
+// ```js
+// const car = {
+//     make: "Toyota",
+//     model: "Corolla",
+//     year: 2020
+// };
+
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020,
+    passenger_capacity:5
+};
+
+console.log(car.passenger_capacity);
